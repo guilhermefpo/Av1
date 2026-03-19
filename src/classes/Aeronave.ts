1fe4f32766dffbd92be32a47676ff20d4c4d593e
Guilherme Fernando Portela de Oliveira <gfernandoportelaoliveira@gmail.com>
2026-03-19 19:44:51 -0300

    Melhorando Aeronave.ts e Funcionario.ts

--- src/classes/Aeronave.ts
+++ src/classes/Aeronave.ts
@@ -1,6 +1,7 @@
 import type { Etapa, Teste } from "./index.js";
 import type Peca from "./Peca.js";
 import { TipoAeronave } from "../enums/TipoAeronave.js";
+import Funcionario from "./Funcionario.js";
 
 export default class Aeronave {
   codigo: string;
@@ -8,9 +9,9 @@ export default class Aeronave {
   tipo: TipoAeronave;
   capacidade: number;
   alcance: number;
-  pecas: Peca[] = [];
-  etapas: Etapa[] = [];
-  testes: Teste[] = [];
+  private _pecas: Peca[] = [];
+  private _etapas: Etapa[] = [];
+  private _testes: Teste[] = [];
 
   constructor(
     codigo: string,
@@ -32,5 +33,47 @@ export default class Aeronave {
     console.log(`Tipo: ${this.tipo}`);
     console.log(`Capacidade: ${this.capacidade}`);
     console.log(`Alcance: ${this.alcance}`);
+  }
+
+  adicionarPecaPorFuncionario(peca: Peca, funcionario: Funcionario) {
+    if (funcionario.nivelPermissao === "ADMINISTRADOR") {
+      this._pecas.push(peca);
+    } else {
+      console.log(
+        "Permissão insuficiente: somente administradores podem adicionar peças.",
+      );
+    }
+  }
+
+  adicionarEtapaPorFuncionario(etapa: Etapa, funcionario: Funcionario) {
+    if (funcionario.nivelPermissao === "ADMINISTRADOR") {
+      this._etapas.push(etapa);
+    } else {
+      console.log(
+        "Permissão insuficiente: somente administradores podem adicionar etapas.",
+      );
+    }
+  }
+
+  adicionarTestePorFuncionario(teste: Teste, funcionario: Funcionario) {
+    if (funcionario.nivelPermissao === "ADMINISTRADOR") {
+      this._testes.push(teste);
+    } else {
+      console.log(
+        "Permissão insuficiente: somente administradores podem adicionar testes.",
+      );
+    }
+  }
+
+  get pecas(): Peca[] {
+    return this._pecas;
+  }
+
+  get etapas(): Etapa[] {
+    return this._etapas;
+  }
+
+  get testes(): Teste[] {
+    return this._testes;
   }
 }
--- src/classes/Funcionario.ts
+++ src/classes/Funcionario.ts
@@ -1,4 +1,7 @@
 import { NivelPermissao } from "../enums/NivelPermissao.js";
+import type Etapa from "./Etapa.js";
+import Peca from "./Peca.js";
+import type Teste from "./Teste.js";
 
 export default class Funcionario {
   id: string;
@@ -8,6 +11,9 @@ export default class Funcionario {
   usuario: string;
   senha: string;
   private _nivelPermissao: NivelPermissao;
+  private _pecas: Peca[] = [];
+  private _etapas: Etapa[] = [];
+  private _testes: Teste[] = [];
 
   constructor(
     id: string,
@@ -29,5 +35,44 @@ export default class Funcionario {
 
   get nivelPermissao(): NivelPermissao {
     return this._nivelPermissao;
+  }
+
+  // Método auxiliar para verificar permissão
+  private verificarPermissao() {
+    if (this._nivelPermissao !== NivelPermissao.ADMINISTRADOR) {
+      console.log("Somente o administrador poderá alterar.");
+      return false;
+    }
+    return true;
+  }
+
+  adicionarPeca(peca: Peca) {
+    if (this.verificarPermissao()) {
+      this._pecas.push(peca);
+    }
+  }
+
+  get pecas(): Peca[] {
+    return this._pecas;
+  }
+
+  adicionarEtapa(etapa: Etapa) {
+    if (this.verificarPermissao()) {
+      this._etapas.push(etapa);
+    }
+  }
+
+  get etapas(): Etapa[] {
+    return this._etapas;
+  }
+
+  adicionarTeste(teste: Teste) {
+    if (this.verificarPermissao()) {
+      this._testes.push(teste);
+    }
+  }
+
+  get testes(): Teste[] {
+    return this._testes;
   }
 }
